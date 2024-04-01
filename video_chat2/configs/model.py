43f952017e96f80d608a049a0c29fa952999af0f
JieyuZ2 <jieyuzhang97@gmail.com>
2024-04-01 12:20:34 -0700

    fix config_bert bugs in videochat

--- video_chat2/configs/model.py
+++ video_chat2/configs/model.py
@@ -2,7 +2,7 @@ TextEncoders = dict()
 TextEncoders["bert"] = dict(
     name="bert_base",
     pretrained="bert-base-uncased",
-    config="configs/config_bert.json",
+    config="./dynabench/models/videoqa_models_library/Video_Chat/video_chat2/configs/config_bert.json",
     d_model=768,
     fusion_layer=9,
 )
